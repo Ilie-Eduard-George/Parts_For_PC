fa3a803ead7c0bf15a5b861aa356aff88e81f0b6
Ilie-Eduard-George <eduardgeo72@gmail.com>
2026-04-20 14:26:37 +0300

    Aplic gitignore

---
+++ index.js
@@ -0,0 +1,145 @@
+const express = require("express");
+const path = require("path");
+const { send } = require("process");
+const fs = require("fs");
+const sass = require("sass");
+
+
+const app = express();
+app.set("view engine", "ejs");
+
+obGlobal={
+    obErori:null,
+    obImagini:null,
+    folderScss: path.join(__dirname,"resurse/scss"),
+    folderCss: path.join(__dirname,"resurse/css"),
+    folderBackup: path.join(__dirname,"backup"),
+}
+
+let vect_foldere=[ "temp", "logs", "backup", "fisiere_uploadate" ]
+for (let folder of vect_foldere){
+    let caleFolder=path.join(__dirname, folder);
+    if (!fs.existsSync(caleFolder)) {
+        fs.mkdirSync(path.join(caleFolder), {recursive:true});   
+    }
+}
+
+console.log("Folder index.js", __dirname);
+console.log("Folder curent (de lucru)", process.cwd());
+console.log("Cale fisier", __filename);
+
+app.use("/resurse", express.static(path.join(__dirname, "resurse")));
+
+app.get("/favicon.ico", function(req, res){
+    res.sendFile(path.join(__dirname,"resurse/imagini/favicon/favicon.ico"))
+});
+
+app.get(["/","/index" , "/home"], function(req, res){
+    res.render("pagini/index", {
+        ip: req.ip
+    });
+});
+
+app.get("/despre", function(req, res){
+    res.render("pagini/despre");
+});
+
+function initErori(){
+    let continut = fs.readFileSync(path.join(__dirname,"resurse/json/erori.json")).toString("utf-8");
+    let erori=obGlobal.obErori=JSON.parse(continut)
+    let err_default=erori.eroare_default
+    err_default.imagine=path.join(erori.cale_baza, err_default.imagine)
+    for (let eroare of erori.info_erori){
+        eroare.imagine=path.join(erori.cale_baza, eroare.imagine)
+    }
+
+}
+initErori()
+
+
+function afisareEroare(res, identificator, titlu, text, imagine){
+    //TO DO cautam eroarea dupa identificator
+    let eroare = obGlobal.obErori.info_erori.find((elem) =>
+        elem.identificator == identificator
+    )
+
+    //daca sunt setate titlu, text, imagine, le folosim, 
+    //altfel folosim cele din fisierul json pentru eroarea gasita
+    //daca nu o gasim, afisam eroarea default
+    let errDefault = obGlobal.obErori.eroare_default
+    if(eroare?.status)
+        res.status(eroare.identificator)
+    res.render("pagini/eroare",{
+        imagine: imagine || eroare?.imagine || errDefault.imagine,
+        titlu: titlu || eroare?.titlu || errDefault.titlu,
+        text: text || eroare?.text || errDefault.text,
+    });
+}
+
+
+app.get("/eroare", function(req, res){
+   afisareEroare(res, 404, "Titlu !!!!")
+});
+
+
+
+
+app.get("/*pagina", function(req, res){
+    console.log("Cale pagina", req.url);
+    if (req.url.startsWith("/resurse") && path.extname(req.url)==""){
+        afisareEroare(res,403);
+        return;
+    }
+    if (path.extname(req.url)==".ejs"){
+        afisareEroare(res,400);
+        return;
+    }
+    try{
+        res.render("pagini"+req.url, function(err, rezRandare){
+            if (err){
+                if (err.message.includes("Failed to lookup view")){
+                    afisareEroare(res,404)
+                }
+                else{
+                    afisareEroare(res);
+                }
+            }
+            else{
+                res.send(rezRandare);
+                console.log("Rezultat randare", rezRandare);
+            }
+        });
+    }
+    catch(err){
+        if (err.message.includes("Cannot find module")){
+            afisareEroare(res,404)
+        }
+        else{
+            afisareEroare(res);
+        }
+    }
+});
+
+
+
+
+
+// app.get("/cale", function(req, res){
+//     console.log("Am primit o cerere GET pe /cale");
+//     res.send("Raspuns la <b style='color: red;'> cerere </b>")
+// });
+
+// app.get("/cale2", function(req, res){
+//     res.write("ceva");
+//     res.write("altceva");
+//     res.end();
+
+// });
+
+
+// app.get("/cale2/:a/:b", function(req, res){
+//     res.send(Number(req.params.a) + Number(req.params.b));
+// });
+
+app.listen(8080)
+console.log("Serverul a pornit!");
